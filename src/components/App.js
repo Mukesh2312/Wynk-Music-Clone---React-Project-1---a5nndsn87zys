76d1a26c21d06f0922438194ae15ff59bd3e0ead
Mukesh2312 <49585682+Mukesh2312@users.noreply.github.com>
2024-01-18 13:14:35 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import "../styles/App.css";
-import './Header';
-import './Home';
+
 import Home from "./Home";
-import './Login';
+
 import Login from "./Login";
-import './Register';
+
 import { BrowserRouter as Router, Routes, Route, navLink } from 'react-router-dom';
 import Register from "./Register";
+import Header from "./Header";
 
 
 
@@ -14,6 +14,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Login" element={<Login />} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,39 @@
 import React from "react";
+import '../styles/Header.css'
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div className="header">
-            Header
-        </div>
+        <header className="header">
+            <div className="header_items_container">
+
+
+                <div className="logo_container">
+                    <NavLink to='/' className="logo">
+
+                        <img src="https://img.icons8.com/color-glass/144/wynk-music.png" alt="logo" />
+                        <h3>
+                            Wynk Music
+                        </h3>
+                    </NavLink>
+
+
+
+                </div>
+                <div className="search_box">
+                    <input type="text" placeholder="Search Music" />
+                </div>
+                <div className="header_links_container">
+                    <ul>
+                        <li>
+                            <NavLink to='/login' className='login_btn'>Login</NavLink>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+
+        </header>
     )
 }
 
