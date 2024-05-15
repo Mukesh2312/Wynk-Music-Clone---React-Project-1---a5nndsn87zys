8911d9d4c3c6db17b5779297dbc4089205347a13
Mukesh2312 <49585682+Mukesh2312@users.noreply.github.com>
2024-05-15 18:16:55 +0530

    subsciption modal working

--- src/components/SubscriptionModal.js
+++ src/components/SubscriptionModal.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../styles/SubModal.css'
-import { FaTimes } from 'react-icons/fa';
+import { FaTimes, FaCheck } from 'react-icons/fa';
 
 function SubscriptionModal({ modalStatus, bodyScrollingControl }) {
     return (
@@ -29,6 +29,42 @@ function SubscriptionModal({ modalStatus, bodyScrollingControl }) {
                                 <div className="items_plans">Benifits</div>
                                 <div className="items_plans">Now</div>
                                 <div className="items_plans">Premium</div>
+                            </div>
+                        </div>
+                        <div className="plans-prices">
+                            <div className="features">
+                                <div className="features-items">Unlimited Streaming</div>
+                                <div className="features-items">Unlimited Downloads</div>
+                                <div className="features-items">Add-free Music</div>
+                            </div>
+                            <div className="plans-now">
+                                <div className="plan-status"><FaCheck /></div>
+                                <div className="plan-status"><FaCheck /></div>
+                                <div className="plan-status"><FaCheck /></div>
+                            </div>
+                            <div className="plans-premium">
+                                <div className="plan-status"><FaCheck /></div>
+                                <div className="plan-status"><FaCheck /></div>
+                                <div className="plan-status"><FaCheck /></div>
+                            </div>
+                        </div>
+                        <div className="choose-plane">
+                            <div className="plans plan1">
+                                <p>Yearly</p>
+
+                                <p><sup>₹999</sup> <strong>₹399</strong></p>
+                                <p>SAVE 60%</p>
+                            </div>
+                            <div className="plans plan2">
+                                <p>3 Months</p>
+                                <p><sup>₹289</sup> <strong>₹129</strong></p>
+                                <p>SAVE 55%</p>
+                            </div>
+                            <div className="plans plan3">
+
+                                <p>Monthly</p>
+                                <p><sup>₹99</sup> <strong>₹49</strong></p>
+                                <p>SAVE 50%</p>
                             </div>
                         </div>
 
